9e8c92941e1add95c37233f7ff3b5d32157c80f9
n0bisuke <rsksound@gmail.com>
2016-03-13 17:38:51 +0900

    update gyazo

---
+++ n0bisuke/app.js
@@ -0,0 +1,42 @@
+'use strict'
+
+if(!process.argv[2]){
+  console.log('Error: Argument shortage');
+  process.exit(1);
+}
+
+const webshot = require('webshot');
+const fs      = require('fs');
+const Gyazo  = require('gyazo-api');
+const TOKEN = require('./config');
+const client = new Gyazo(TOKEN);
+const FILENAME = 'output.png';
+const TARGET_URL = process.argv[2];
+
+let renderStream = webshot(TARGET_URL);
+let file = fs.createWriteStream(FILENAME, {encoding: 'binary'});
+
+renderStream.on('data', (data) => {
+  file.write(data.toString('binary'), 'binary');
+});
+
+renderStream.on('end', (data) => {
+  console.log('image saved');
+  gyazoUpload();
+});
+
+function gyazoUpload(){
+  client.upload(FILENAME, {
+    title: "my picture",
+    desc: "upload from nodejs"
+  })
+  .then((res) => {
+    console.log(TARGET_URL);
+    console.log(res.data.image_id);
+    console.log(res.data.permalink_url);
+    console.log(res.data.url);
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+}
